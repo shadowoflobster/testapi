a6c1d0da0e389f18c028b716f7a58bfbb4d187b0
shadowoflobster <guraspishvilileqso@gmail.com>
2025-07-24 16:17:35 +0400

    First commit

--- src/main/java/org/example/HelloController.java
+++ src/main/java/org/example/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Collections;
 import java.util.Map;
 
 @RestController
@@ -12,6 +13,6 @@ public class HelloController {
 
     @GetMapping("/joke")
     public Map<String, String> getJoke() {
-        return Map.of("joke", "Why did the Java developer go broke? Because he used up all his cache!");
+        return Collections.singletonMap("joke", "Why did the Java developer go broke? Because he used up all his cache!ruuuuuuuuuuun!!!");
     }
 }
